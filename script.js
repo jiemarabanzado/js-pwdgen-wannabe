d87fcb4fb2d01a832d7463abbb67bf8fb910e35a
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-19 16:30:11 +0200

    es completo

---
+++ script.js
@@ -0,0 +1,11 @@
+const nome = prompt("inserisci il nome");
+document.getElementById('nome').innerHTML= nome;
+
+const cognome = prompt("inserisci il cognome");
+document.getElementById('cognome').innerHTML= cognome;
+
+const colore = prompt("inserisci il colore preferito");
+document.getElementById('colore').innerHTML= colore;
+
+const password = `${nome}${cognome}${colore}76 `;
+document.getElementById('password').innerHTML= password;
