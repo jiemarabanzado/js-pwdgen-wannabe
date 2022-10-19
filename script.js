e5400301a2180c7a5c280a08e2a94645773922fc
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-19 16:45:17 +0200

    extra

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const nome = prompt("inserisci il nome");
-document.getElementById('nome').innerHTML= nome;
+document.querySelector('#buy').addEventListener('click', genera);
+function genera(){   
+    const nome = prompt("inserisci il nome");
+    document.getElementById('nome').innerHTML= nome;
 
-const cognome = prompt("inserisci il cognome");
-document.getElementById('cognome').innerHTML= cognome;
+    const cognome = prompt("inserisci il cognome");
+    document.getElementById('cognome').innerHTML= cognome;
 
-const colore = prompt("inserisci il colore preferito");
-document.getElementById('colore').innerHTML= colore;
+    const colore = prompt("inserisci il colore preferito");
+    document.getElementById('colore').innerHTML= colore;
 
-const password = `${nome}${cognome}${colore}76 `;
-document.getElementById('password').innerHTML= password;
+    const password = `${nome}${cognome}${colore}76 `;
+    document.getElementById('password').innerHTML= password;
+}
